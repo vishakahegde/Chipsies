33d1e481c83179f99e7b11486a1a21442ee51e90
Mitraws <m.ws@outlook.com>
2020-05-19 16:08:02 +0200

    Add NAvbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+return (
+    <div>
+    <NavLink activeStyle={{background: "lightpink"}} exact to="/">Home</NavLink>{" | "}
+    <NavLink activeStyle={{background: "lightpink"}} to="/CartPage">Cart</NavLink>{" | "}
+    <NavLink activeStyle={{background: "lightpink"}} to="/ProductPage">Products</NavLink>{" | "}
+    </div>
+)
+}
