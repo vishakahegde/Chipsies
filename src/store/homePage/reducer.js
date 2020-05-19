5c95de191db193acb09ec7aed159c88cb1ab4d2a
vishakahegde <hegde.vishaka@gmail.com>
2020-05-19 17:58:28 +0200

    Imported db.json into reducer

---
+++ src/store/homePage/reducer.js
@@ -0,0 +1,9 @@
+const initialState = require("../../../db");
+
+export default function homePageReducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
