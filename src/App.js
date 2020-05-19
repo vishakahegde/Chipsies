186300bb58306c763b50a601d65dac7ad5bdaa83
Mitraws <m.ws@outlook.com>
2020-05-19 17:03:15 +0200

    Add switch to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
-import Navbar from "./components/Navbar"
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Switch, Route } from "react-router-dom";
+import CartPage from "./Pages/CartPage";
+import HomePage from "./Pages/HomePage";
+import ProductPage from "./Pages/ProductPage";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-<hr></hr>
+      <Navbar />
+      {/* <hr></hr>
+      <Switch>
+        <Route path="/CartPage" component={CartPage}/>
+        <Route path="/ProductPage" component={ProductPage}/>
+        <Route exact path="/" component={HomePage}/>
+      </Switch> */}
     </div>
   );
 }
