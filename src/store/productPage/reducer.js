9f4b7071b84872c3d6c83fe2ad52e1a01b1586cb
Mitraws <m.ws@outlook.com>
2020-05-19 18:58:32 +0200

    Debug productPage reducer

--- src/store/productPage/reducer.js
+++ src/store/productPage/reducer.js
@@ -1,47 +1,46 @@
 const initialState = 
 [
     {
-      "id": 1,
-      "title": "flowers",
-      "categories": "Good",
-      "price": "€ 5",
-      "popularity": 4
+      id: 1,
+      title: "flowers",
+      categories: "Good",
+      price: "€ 5",
+      popularity: 4
     },
     {
-      "id": 2,
-      "title": "grasses",
-      "categories": "Bad",
-      "price": "€ 3",
-      "popularity": 2
+      id: 2,
+      title: "grasses",
+      categories: "Bad",
+      price: "€ 3",
+      popularity: 2
     },
     {
-      "id": 3,
-      "title": "spices",
-      "categories": "Worse",
-      "price": "€ 6",
-      "popularity": 5
+      id: 3,
+      title: "spices",
+      categories: "Worse",
+      price: "€ 6",
+      popularity: 5
     },
     {
-      "id": 4,
-      "title": "gums",
-      "categories": "Best",
-      "price": "€ 5",
-      "popularity": 1
+      id: 4,
+      title: "gums",
+      categories: "Best",
+      price: "€ 5",
+      popularity: 1
     },
     {
-      "id": 5,
-      "title": "balsams",
-      "categories": "Mediocre",
-      "price": "€ 3",
-      "popularity": 3
+      id: 5,
+      title: "balsams",
+      categories: "Mediocre",
+      price: "€ 3",
+      popularity: 3
     }
-  ],
+  ];
 
-export default function productPageSliceReducer(state = initialState, action) {
-  console.log("action?", action, "state?", state)
-  switch (action.type) {
-    default: {
-      return state;
+  export default function productPageSliceReducer(state = initialState, action) {
+    switch (action.type) {
+      default: {
+        return state;
+      }
     }
   }
-}
