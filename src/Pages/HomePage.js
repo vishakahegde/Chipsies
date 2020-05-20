fb7eef64837b2efa1be55b0ad776530222ecd13a
vishakahegde <hegde.vishaka@gmail.com>
2020-05-20 15:15:43 +0200

    Added filter  by price, popularity & categories

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,41 +5,92 @@ import React, { useEffect, useState } from "react";
 // } from "../store/homePage/selector";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
-import { fetchData } from "../store/productPage/action";
+// import { fetchData } from "../store/productPage/action";
 
 export default function HomePage() {
   const dispatch = useDispatch();
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [category, setCategory] = useState("All");
 
   useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
+    async function fetchData() {
+      const response = await axios.get(
+        `https://my-json-server.typicode.com/vishakahegde/Chipsies/products`
+      );
+      setData(response.data);
+    }
+    fetchData();
+  }, []);
 
-  // function compare_popularity(product_a, product_b) {
-  //   return product_b.popularity - product_a.popularity;
-  // }
-  // function sortedByPopularity() {
-  //   const sortedData = data.sort(compare_popularity);
-  //   setData(sortedData);
-  // }
+  // useEffect(() => {
+  //   dispatch(fetchData());
+  // }, [dispatch]);
 
-  // console.log("Data", data);
+  function compare_popularity(product_a, product_b) {
+    return product_b.popularity - product_a.popularity;
+  }
+  function sortedByPopularity() {
+    const sortedData = [...data].sort(compare_popularity);
+    setData(sortedData);
+  }
+
+  function compare_price(product_a, product_b) {
+    let price_a = product_a.price.split(" ")[1];
+    let price_b = product_b.price.split(" ")[1];
+
+    return price_b - price_a;
+  }
+
+  function sortByPrice() {
+    const sortedData = [...data].sort(compare_price);
+    setData(sortedData);
+  }
+
+  function ProductByCategory(category) {
+    setCategory(category);
+    // console.log("Data", category);
+    // if (category !== "All") {
+    //   const newData = data.filter((product) => {
+    //     return product.categories === category;
+    //   });
+    //   setData(newData);
+    // }
+  }
 
   return (
     <div>
       <h1>Chips</h1>
-      {/* <ul>
+      <ul>
         {data.map((product) => {
-          return (
-            <div>
-              <li>Product Name: {product.title}</li>
-              <li>Price: {product.price}</li>
-              <li>Popularity: {product.popularity}</li>
-            </div>
-          );
+          if (category === "All") {
+            return (
+              <div>
+                <li>Product Name: {product.title}</li>
+                <li>Price: {product.price}</li>
+                <li>Popularity: {product.popularity}</li>
+              </div>
+            );
+          } else if (product.categories === category) {
+            return (
+              <div>
+                <li>Product Name: {product.title}</li>
+                <li>Price: {product.price}</li>
+                <li>Popularity: {product.popularity}</li>
+              </div>
+            );
+          }
         })}
       </ul>
-      <button onClick={sortedByPopularity}>Sort By Popularity</button> */}
+      <button onClick={sortedByPopularity}>Sort By Popularity</button>
+      <button onClick={sortByPrice}>Sort By Price</button>
+      <select onChange={(event) => ProductByCategory(event.target.value)}>
+        <option value="All">All</option>
+        {data.map((product) => {
+          return (
+            <option value={product.categories}>{product.categories}</option>
+          );
+        })}
+      </select>
     </div>
   );
 }
