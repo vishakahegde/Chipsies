b886a624da8744e279f7f987ec4dd4debcfe5193
Mitraws <m.ws@outlook.com>
2020-05-20 19:21:18 +0200

    Add hr in app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <hr></hr>
       <Switch>
         <Route path="/CartPage" component={CartPage} />
         <Route path="/ProductPage/:productId" component={ProductPage} />
