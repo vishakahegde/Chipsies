9c9a94b5bb3602960f7c317b08c03008f8473afa
vishakahegde <hegde.vishaka@gmail.com>
2020-05-20 09:23:28 +0200

    Fixed by doing a master pull and content for homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { showProducts } from "../store/homePage/selector";
+import { useSelector } from "react-redux";
 
 export default function HomePage() {
+  const products = useSelector(showProducts);
+  console.log("Product:", products);
+
   return (
     <div>
       <h1>Chips</h1>
+      {products.map((product) => {
+        return <p>{product.title}</p>;
+      })}
     </div>
   );
 }
