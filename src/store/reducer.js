fe2a1a3d8b9b492af674abfa01ce2344d7b641fc
vishakahegde <hegde.vishaka@gmail.com>
2020-05-20 09:04:02 +0200

    created combined reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,20 @@
-const initialState = 0;
+// const initialState = 0;
 
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    default: {
-      return state;
-    }
-  }
-}
+// export default function reducer(state = initialState, action) {
+//   switch (action.type) {
+//     default: {
+//       return state;
+//     }
+//   }
+// }
+
+import { combineReducers } from "redux";
+import homePageReducer from "./homePage/reducer";
+
+console.log("HomePage reducer", homePageReducer);
+
+const reducer = combineReducers({
+  homePage: homePageReducer,
+});
+
+export default reducer;
