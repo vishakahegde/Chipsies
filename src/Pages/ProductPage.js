17c7c4497d4cbce665946d9a01aa8ba545d364ac
Mitraws <m.ws@outlook.com>
2020-05-20 13:25:15 +0200

    Fetch data product page

--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -1,26 +1,50 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
-// const API_URL = `http://vmy-json-server.typicode.com/vishakahegde/Chipsies`;
-
+import { useParams } from "react-router-dom";
+// import { useSelector } from "react-redux";
+// import {selectProduct} from "../store/productPage/selector"
+// import { Link } from "react-router-dom";
 
 export default function ProductPage() {
-    const [chips, setChips] = useState([]);
-    
-    //   useEffect(() => {
-    //     const fetchData = async ()  => {
-    //        setChips({loading: true });
-    //        const data = await axios.get(
-    //          `TBD`
-    //        );
-    //        setChips();
-    //      }
-    //      fetchData(); 
-    //    }, []);
+  const params = useParams();
+  const [chips, setChips] = useState([]);
+  const [loadingData, setLoadingData] = useState(false);
+
+  // console.log("param:", params)
+
+  useEffect(() => {
+    const fetchData = async () => {
+         setLoadingData(true);
+      const data = await axios.get(
+        "http://my-json-server.typicode.com/vishakahegde/Chipsies/products"
+      );
+      console.log("data:", data.data);
+
+         setChips(data.data);
+         setLoadingData(false)
+    };
+    fetchData();
+  }, []);
+
+  const loading = loadingData === true ? <h1>Loading...</h1> : null
+
+  const filteredChips = chips.filter(chip => {
+      console.log("chip?", chip)
+
+    if (params === chip.title) {
+        return true
+    } else {
+        return false
+    }
+  })
+  console.log("filter ok?", filteredChips)
+  
 
   return (
     <div>
-      <h1>Product Page</h1>
+      {filteredChips}
+      {loading}
+      
     </div>
   );
 }
